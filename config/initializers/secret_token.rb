38765f77cf83c31d41a004c697f252b531243052
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-10-09 17:16:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PepperjamChallenge::Application.config.secret_key_base = '815a5da944375b9a08e704ac2b711431d2a8845550b2e268cad24cbed1caebe441182c2111e7a2a276f25b49d65c0933c6fbdec5473c170872dd1b1d656eaff2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PepperjamChallenge::Application.config.session_store :cookie_store, key: '_pepperjam_challenge_session'
